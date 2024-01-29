463ee33288405e392c170f00843f4a30f875ac29
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2024-01-29 14:01:35 +0100

    Feat: Dissolve shader script

---
+++ BioKid/Assets/Animations/Dissolve/DissolveScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveScript : MonoBehaviour
+{
+    
+    public Material dissolveMaterial;
+
+
+
+
+    void Start()
+    {
+        
+
+        dissolveMaterial = GetComponent<Material>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
